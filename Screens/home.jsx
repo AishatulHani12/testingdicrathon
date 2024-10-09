b24f7976fc41b817a52c4a25bc27f60045eae5a2
AishatulHani12 <d20221101861@m365.upsi.edu.my>
2024-10-09 13:39:10 +0800

    SecondCommit

--- Screens/home.jsx
+++ Screens/home.jsx
@@ -1,14 +1,67 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView, Image, Pressable} from 'react-native'
 
-export class home extends Component {
+export class Home extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <View className="flex-1 items-center justify-start py-10 ml-10">
+      <ScrollView className="-mx-20 pl-46" >
+        <View className="w-80 h-16 bg-emerald-700 rounded-lg ml-7">
+          <View className="flex-row">
+            <Image className="w-10 h-10 top-3 left-3 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+            <Text className="text-left left-4 top-4 text-3xl font-extrabold ">HikeMate</Text>
+          </View>
+        </View>
+        <View className="w-screen">
+          <Image className="w-full h-52 rounded-sm  " resizeMode="contain" source={require("../assets/image1.png")} ></Image>
+        </View>
+        <View className="ml-5">
+          <Text className=" text-2xl font-extrabold">Activity</Text>
+        </View>
+        <View className="flex-row top-4 left-1 ml-4">
+        
+
+          <View className="w-20 h-20 bg-emerald-600 rounded-lg  mr-1 ">
+          <Pressable onPress={() => Alert.alert('Pressed')}>
+            <Image className="w-10 h-10 top-5 left-5 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+  </Pressable>
+          </View>
+          
+          <View className="w-20 h-20 bg-emerald-600 rounded-lg  mr-1 ">
+          <Pressable onPress={() => Alert.alert('Pressed')}>
+            <Image className="w-10 h-10 top-5 left-5 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+          </Pressable>
+          </View>
+          <View className="w-20 h-20 bg-emerald-600 rounded-lg  mr-1 ">
+            <Image className="w-10 h-10 top-5 left-5 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+          </View>
+          <View className="w-20 h-20 bg-emerald-600 rounded-lg mr-1 ">
+            <Image className="w-10 h-10 top-5 left-5 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+          </View>
+        </View>
+        <View className="top-4 flex-row right-4">
+          <Text className="text-center left-6 w-24 text-sm font-normal">Hike</Text>
+          <Text className="text-center left-8 w-16 mr-4 text-sm font-normal">Camp</Text>
+          <Text className="text-center left-5 w-24 text-sm font-normal">Trails</Text>
+          <Text className="text-center left-2 w-24 text-sm font-normal">Outdoor Activities</Text>
+        </View>
+        <View className="top-7 ml-5">
+          <Text className=" text-left text-2xl font-extrabold">Review</Text>
+        </View>
+        <View className="flex-row ">
+          <Image className="w-10 h-10 left-5 top-10 rounded-full" source={require("../assets/penguindp.jpeg")} ></Image>
+        </View>
+        <View className=" left-28  ">
+          <Text className="text-sm font-semibold">Lisa Racher</Text>
+          <Text className="text-sm  font-normal text-gray-500">Mount Kinabalu is one of the most popular hiking destinations in Sabah.</Text>
+        </View>
+        <View className="flex-row justify-end ">
+          <Image className="w-60 h-50 right-6 -top-4 rounded-lg" source={require("../assets/penguindp.jpeg")} ></Image>
+        </View>
+      </ScrollView>
+    </View>
     )
   }
 }
 
-export default home
+export default Home
